8c3ae979bd68f6edd8ec3e6f5f26906adf2ba939
lunatik752 <whitemaxim752@gmail.com>
2020-11-08 19:17:16 +0300

    add api and map rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback, useState} from 'react';
 import './App.css';
+import {Map, Placemark, SearchControl, YMaps} from "react-yandex-maps";
+import {getDataRequest} from "./api/api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+
+    const getData = useCallback(async () => {
+        const res = await getDataRequest('минск')
+        console.log(res)
+}, [])
+
+    return (
+        <div className="App">
+            <YMaps>
+                <div>My awesome application with maps!</div>
+                <div style={{height: '100vh', width: '100%'}}>
+                    <Map defaultState={{center: [53.90, 27.56], zoom: 12}} width={'100%'} height={'100vh'}>
+                        <SearchControl options={{ float: 'right' }} />
+                        <Placemark geometry={[53.917501, 27.604851]}/>
+                    </Map>
+                </div>
+            </YMaps>
+        </div>
+    )
 }
+
 
 export default App;
